901ccfe9e73aa9a9cada13daa5ea0ed2f9bb2e48
twomotive <112391727+twomotive@users.noreply.github.com>
2025-03-08 11:16:42 +0300

    feat: added cell class

---
+++ cell.py
@@ -0,0 +1,26 @@
+from graphics import *
+class Cell:
+    def __init__(self,win:Window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        
+    def draw(self,x1,y1,x2,y2):
+        if self.has_left_wall:
+            line = Line(Point(x1,y1),Point(x1,y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2,y1),Point(x2,y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1,y1),Point(x2,y1))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1,y2),Point(x2,y2))
+            self._win.draw_line(line)
