0fd42917fc7bea2d96000559fddd552058d2d563
twomotive <112391727+twomotive@users.noreply.github.com>
2025-03-08 11:33:53 +0300

    feat: draw_move method added to Cell class
    This is for path tracing.

--- cell.py
+++ cell.py
@@ -12,6 +12,11 @@ class Cell:
         self._win = win
         
     def draw(self,x1,y1,x2,y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        
         if self.has_left_wall:
             line = Line(Point(x1,y1),Point(x1,y2))
             self._win.draw_line(line)
@@ -24,3 +29,16 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1,y2),Point(x2,y2))
             self._win.draw_line(line)
+            
+    def draw_move(self,to_cell,undo=False):
+        line_color = "gray" if undo else "red"
+        
+        
+        center_x1 = (self._x1 + self._x2) // 2
+        center_y1 = (self._y1 + self._y2) // 2
+        center_x2 = (to_cell._x1 + to_cell._x2) // 2
+        center_y2 = (to_cell._y1 + to_cell._y2) // 2
+        
+        
+        line = Line(Point(center_x1, center_y1), Point(center_x2, center_y2))
+        self._win.draw_line(line, line_color)
