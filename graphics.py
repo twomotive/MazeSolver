12d4136b5425379193ae3c154f9301429f60b759
twomotive <112391727+twomotive@users.noreply.github.com>
2025-03-08 11:04:25 +0300

    feat: Add Point and Line classes with draw_line method

--- graphics.py
+++ graphics.py
@@ -21,3 +21,21 @@ class Window:
             
     def close(self):
         self.__running = False
+        
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+        
+        
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        
+    def draw(self,canvas,fill_color="black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color)
+        
