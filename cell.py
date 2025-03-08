a367a1d4f6a0a85e9cd5551afdfe646b7885b3a8
twomotive <112391727+twomotive@users.noreply.github.com>
2025-03-08 12:25:12 +0300

    feat: added remove wall logic

--- cell.py
+++ cell.py
@@ -1,6 +1,6 @@
 from graphics import *
 class Cell:
-    def __init__(self,win:Window):
+    def __init__(self,win:Window=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -11,24 +11,37 @@ class Cell:
         self._y2 = None
         self._win = win
         
-    def draw(self,x1,y1,x2,y2):
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        
         if self.has_left_wall:
-            line = Line(Point(x1,y1),Point(x1,y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
-        if self.has_right_wall:
-            line = Line(Point(x2,y1),Point(x2,y2))
-            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
-            line = Line(Point(x1,y1),Point(x2,y1))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
-            line = Line(Point(x1,y2),Point(x2,y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
             
     def draw_move(self,to_cell,undo=False):
         line_color = "gray" if undo else "red"
